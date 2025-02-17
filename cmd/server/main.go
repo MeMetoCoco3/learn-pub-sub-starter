4992409db85e01bdfa9cd348d2f9612dbce427f9
MeMetoCoco3 <torresmartinezvidal@gmail.com>
2025-02-17 20:28:39 +0100

    lets goo!

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	log.Println("Starting Peril server...")
+
+	connectionString := "amqp://guest:guest@localhost:5672/"
+	con, err := amqp.Dial(connectionString)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer con.Close()
+
+	log.Println("Successfully connected")
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	sig := <-signalChan
+	log.Printf("Signal was recieved: %s\n", sig.String())
+	con.Close()
+
+	os.Exit(0)
 }
