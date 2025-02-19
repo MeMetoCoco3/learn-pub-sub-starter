6e5132dbaf935e28a03a238095acf057357a0e4e
MeMetoCoco3 <torresmartinezvidal@gmail.com>
2025-02-19 22:51:44 +0100

    Publish json message on exchange

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	ps "github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+
+	routing "github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
 	"log"
 	"os"
 	"os/signal"
-
-	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 func main() {
@@ -19,11 +21,18 @@ func main() {
 	defer con.Close()
 
 	log.Println("Successfully connected")
+
+	chanel, err := con.Channel()
+	if err != nil {
+		log.Fatalf("Error creating new channel: %s", err)
+	}
+	data := routing.PlayingState{IsPaused: true}
+	ps.PublishJson(chanel, routing.ExchangePerilDirect, routing.PauseKey, data)
+
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, os.Interrupt)
 	sig := <-signalChan
 	log.Printf("Signal was recieved: %s\n", sig.String())
-	con.Close()
 
-	os.Exit(0)
+	return
 }
